21ab37e2e2ddb3671c3d0d87930b88c7aa2690b5
b3liott <eliott.georges8@gmail.com>
2024-12-06 18:40:11 +0100

    desc

--- quarantine/war-quarantine.go
+++ quarantine/war-quarantine.go
@@ -4,6 +4,10 @@ package quanrantine
 // Author: b3liott
 // Date: 2024-12-04
 // License: MIT
+// Description: This active response puts in
+//				quarantine the concerned computer
+// 				It closes every ports, kills most
+//				services and dumps the memory
 // =============================================
 
 // QUARANTINE ACTIVE RESPONSE
